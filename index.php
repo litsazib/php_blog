9e5cef0566758653f9cca6efb376cd851629d220
litsazib <roktimsazib92@gmail.com>
2016-11-13 13:37:03 +0600

    Fast commit

---
+++ index.php
@@ -0,0 +1,66 @@
+<?php require_once('header.php'); ?>
+<div class="page_title1 sty11">
+<div class="container">
+	<img src="images/web-developers.png"> 
+</div>	    
+</div><!-- end page title -->
+<div class="clearfix margin_bottom5"></div>
+<div class="container">
+<div class="row">
+  <h1 class="center_s">Our Blog</h1>
+  <div class="col-md-3">
+  <div class="sitebar blogs">
+    <h1>OUR SERVICE</h1>
+      <ul>
+        <li><a href="#">Category One</a></li>
+        <li><a href="#">Category Two</a></li>
+        <li><a href="#">Category Three</a></li>
+        <li><a href="#">Category Four</a></li>
+        <li><a href="#">Category Five</a></li>            
+      </ul>
+  </div>
+  <div class="sitebar blogs">
+    <h1>NEWS CATEGORY</h1>
+      <ul>
+        <li><a href="#">Category One</a></li>
+        <li><a href="#">Category Two</a></li>
+        <li><a href="#">Category Three</a></li>
+        <li><a href="#">Category Four</a></li>
+        <li><a href="#">Category Five</a></li>            
+      </ul>
+  </div>
+  <div class="sitebar blogs">
+  <h1>RECENT NEWS</h1>
+    <ul>
+      <li><a href="#">Category One</a></li>
+      <li><a href="#">Category Two</a></li>
+      <li><a href="#">Category Three</a></li>
+      <li><a href="#">Category Four</a></li>
+      <li><a href="#">Category Five</a></li>            
+    </ul>
+  </div>
+
+  </div>
+  <div class="col-md-9">
+    <div class="blogs">    
+      <h1>Scan Bangladesh launching<!-- <span>01 November 2016</span> --></h1>
+      <h2>Posted :<span>Adimnestrator</span> </h2>
+      <div class="left">
+      <a href=""><img src="images/seo-c.jpg" alt=""></a>       
+      </div>
+      <p>Adheres to development deadlines and schedules. Understands the necessity of and contributes to coding standards. Should be able to prioritize multiple tasks effectively. Encourages other employees to take responsibility for their development within the company. Create Unit Test Cases Should be able to work on .Net Technologies like Web services, Windows Services, ADO.Net and Reflection. Expertise in using Web developments tools such as HTML, JavaScript, XML, JQuery and AJAX. ASP.Net/VB.net Knowledge added advantage. Write SQL Queries, Stored Procedures, Triggers and Views using SQL Understands and Implements Microsoft Patterns and Practices across applications Good knowlegde in CSS will be an advantage. Should have good analytical, communication and interpersonal skills.</p>
+      <a class="apply_now" href="">Read More...</a>
+    </div>
+        <div class="blogs">    
+      <h1>Scan Bangladesh launching<!-- <span>01 November 2016</span> --></h1>
+      <h2>Posted :<span>Adimnestrator</span> </h2>
+      <div class="left">
+      <a href=""><img src="images/seo-c.jpg" alt=""></a>       
+      </div>
+      <p>Adheres to development deadlines and schedules. Understands the necessity of and contributes to coding standards. Should be able to prioritize multiple tasks effectively. Encourages other employees to take responsibility for their development within the company. Create Unit Test Cases Should be able to work on .Net Technologies like Web services, Windows Services, ADO.Net and Reflection. Expertise in using Web developments tools such as HTML, JavaScript, XML, JQuery and AJAX. ASP.Net/VB.net Knowledge added advantage. Write SQL Queries, Stored Procedures, Triggers and Views using SQL Understands and Implements Microsoft Patterns and Practices across applications Good knowlegde in CSS will be an advantage. Should have good analytical, communication and interpersonal skills.</p>
+      <a class="apply_now" href="">Read More...</a>
+    </div>
+  </div>
+</div>
+</div>
+<?php require_once('footer.php') ?>
---
+++ single-post.php
@@ -0,0 +1,85 @@
+<?php require_once('header.php'); ?>
+<div class="page_title1 sty11">
+<div class="container">
+	<img src="images/web-developers.png"> 
+</div>	    
+</div><!-- end page title -->
+<div class="clearfix margin_bottom5"></div>
+<div class="container">
+<div class="row">
+  <h1 class="center_s">Our Blog</h1>
+  <div class="col-md-3">
+  <div class="sitebar blogs">
+    <h1>OUR SERVICE</h1>
+      <ul>
+        <li><a href="#">Category One</a></li>
+        <li><a href="#">Category Two</a></li>
+        <li><a href="#">Category Three</a></li>
+        <li><a href="#">Category Four</a></li>
+        <li><a href="#">Category Five</a></li>            
+      </ul>
+  </div>
+  <div class="sitebar blogs">
+    <h1>NEWS CATEGORY</h1>
+      <ul>
+        <li><a href="#">Category One</a></li>
+        <li><a href="#">Category Two</a></li>
+        <li><a href="#">Category Three</a></li>
+        <li><a href="#">Category Four</a></li>
+        <li><a href="#">Category Five</a></li>            
+      </ul>
+  </div>
+  <div class="sitebar blogs">
+  <h1>RECENT NEWS</h1>
+    <ul>
+      <li><a href="#">Category One</a></li>
+      <li><a href="#">Category Two</a></li>
+      <li><a href="#">Category Three</a></li>
+      <li><a href="#">Category Four</a></li>
+      <li><a href="#">Category Five</a></li>            
+    </ul>
+  </div>
+
+  </div>
+  <div class="col-md-9">
+    <div class="blogs">    
+      <h1>Scan Bangladesh launching<!-- <span>01 November 2016</span> --></h1>
+      <h2>Posted :<span>Adimnestrator</span> </h2>
+      <div class="left">
+      <a href=""><img src="images/seo-c.jpg" alt=""></a>       
+      </div>
+      <p>Adheres to development deadlines and schedules. Understands the necessity of and contributes to coding standards. Should be able to prioritize multiple tasks effectively. Encourages other employees to take responsibility for their development within the company. Create Unit Test Cases Should be able to work on .Net Technologies like Web services, Windows Services, ADO.Net and Reflection. Expertise in using Web developments tools such as HTML, JavaScript, XML, JQuery and AJAX. ASP.Net/VB.net Knowledge added advantage. Write SQL Queries, Stored Procedures, Triggers and Views using SQL Understands and Implements Microsoft Patterns and Practices across applications Good knowlegde in CSS will be an advantage. Should have good analytical, communication and interpersonal skills.</p>
+      <a class="apply_now" href="">Read More...</a>
+    </div>
+        <div class="blogs">    
+      <h1>Scan Bangladesh launching<!-- <span>01 November 2016</span> --></h1>
+      <h2>Posted :<span>Adimnestrator</span> </h2>
+      <div class="left">
+      <a href=""><img src="images/seo-c.jpg" alt=""></a>       
+      </div>
+      <p>Adheres to development deadlines and schedules. Understands the necessity of and contributes to coding standards. Should be able to prioritize multiple tasks effectively. Encourages other employees to take responsibility for their development within the company. Create Unit Test Cases Should be able to work on .Net Technologies like Web services, Windows Services, ADO.Net and Reflection. Expertise in using Web developments tools such as HTML, JavaScript, XML, JQuery and AJAX. ASP.Net/VB.net Knowledge added advantage. Write SQL Queries, Stored Procedures, Triggers and Views using SQL Understands and Implements Microsoft Patterns and Practices across applications Good knowlegde in CSS will be an advantage. Should have good analytical, communication and interpersonal skills.</p>
+      <a class="apply_now" href="">Read More...</a>
+    </div>
+  </div>
+</div>
+<!-- START RELATED POST -->
+  <div class="container">
+    <h1 class="center_s">RELATED POST</h1>
+ <div id="owl-demo" class="owl-carousel">    
+        <div class="item"><img src="images/html.jpg" alt="" /></div>        
+        <div class="item"><img src="images/php.png" alt="" /></div>        
+        <div class="item"><img src="images/mongodb.png" alt="" /></div>        
+        <div class="item"><img src="images/angular.png" alt="" /></div>   
+         <div class="item"><img src="images/node.png" alt="" /></div>        
+        <div class="item"><img src="images/larable.png" alt="" /></div>        
+        <div class="item"><img src="images/gulp.jpg" alt="" /></div>        
+        <div class="item"><img src="images/codiginater.jpg" alt="" /></div>
+        <div class="item"><img src="images/wp.png" alt="" /></div> 
+        <div class="item"><img src="images/git.png" alt="" /></div>        
+        <div class="item"><img src="images/less-sass.jpg" alt="" /></div>
+        <div class="item"><img src="images/phonegap.png" alt="" /></div> 
+        <div class="item"><img src="images/drupal.png" alt="" /></div>        
+    </div>  
+  </div>
+</div>
+<?php require_once('footer.php') ?>
